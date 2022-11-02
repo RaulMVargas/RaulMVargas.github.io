617e836bca7b30af47ac73916ebcd50a60909161
Raul Vargas <58316069+RaulMVargas@users.noreply.github.com>
2022-11-01 21:08:45 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+https://github.com/RaulMVargas/RaulMVargas.github.io/tree/main/HW3%20GUI
+
+https://raulmvargas.github.io/
---
+++ js/style.js
@@ -0,0 +1,34 @@
+/*
+Raul letgas
+Javascript file for HW3
+*/
+
+generateTable = () => {
+    //import all the values from the html form
+    let minColumn = document.getElementById("minColumn").value;
+    let maxColumn = document.getElementById("maxColumn").value;
+    let minRow = document.getElementById("minRow").value;
+    let maxRow = document.getElementById("maxRow").value;
+    
+    //get an id for the table and create a blank output
+    let table = document.getElementById('table');
+    let output="";
+    
+    //nested for loops to iterate through the edges and then fill out the table
+    for(let i = minRow; i <= maxRow; i++) {
+        if (i == minRow) {
+            output+="<tr>" + "<td></td>";
+            for(let j = minColumn; j <= maxColumn; j++) {//second for loop to do the outside columns
+                output += "<td>"+ j +"</td>";
+            }
+            output+="</tr>";
+        }
+        output+="<tr>" + "<td>" + i + "</td>";
+        for(let j = minColumn; j <= maxColumn; j++) {//third and final for loop to print out the values
+            output += "<td>"+ i*j +"</td>";
+        }
+         output+="</tr>"
+    }
+    //set everything we have so far in the output to the html table
+    table.innerHTML=output;
+}
