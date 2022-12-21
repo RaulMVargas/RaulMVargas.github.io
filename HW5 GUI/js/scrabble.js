a27b417ccf5e030f441f6d798e4192253d29a53d
Raul Vargas <58316069+RaulMVargas@users.noreply.github.com>
2022-12-20 19:17:05 -0500

    Add files via upload

---
+++ HW5 GUI/js/scrabble.js
@@ -0,0 +1,126 @@
+let scrabbleTiles = [] ;
+scrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9, "image" : "tileImages/scrabble/Scrabble_Tile_A.jpg"  } ;
+scrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_B.jpg"  } ;
+scrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_C.jpg"  } ;
+scrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4, "image" : "tileImages/scrabble/Scrabble_Tile_D.jpg"  } ;
+scrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12, "image" : "tileImages/scrabble/Scrabble_Tile_E.jpg"  } ;
+scrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_F.jpg"  } ;
+scrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3, "image" : "tileImages/scrabble/Scrabble_Tile_G.jpg"  } ;
+scrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_H.jpg"  } ;
+scrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9, "image" : "tileImages/scrabble/Scrabble_Tile_I.jpg"  } ;
+scrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1, "image" : "tileImages/scrabble/Scrabble_Tile_J.jpg"  } ;
+scrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1, "image" : "tileImages/scrabble/Scrabble_Tile_K.jpg"  } ;
+scrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4, "image" : "tileImages/scrabble/Scrabble_Tile_L.jpg"  } ;
+scrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_M.jpg"  } ;
+scrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6, "image" : "tileImages/scrabble/Scrabble_Tile_N.jpg"  } ;
+scrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8, "image" : "tileImages/scrabble/Scrabble_Tile_O.jpg"  } ;
+scrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_P.jpg"  } ;
+scrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1, "image" : "tileImages/scrabble/Scrabble_Tile_Q.jpg"  } ;
+scrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6, "image" : "tileImages/scrabble/Scrabble_Tile_R.jpg"  } ;
+scrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4, "image" : "tileImages/scrabble/Scrabble_Tile_S.jpg"  } ;
+scrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6, "image" : "tileImages/scrabble/Scrabble_Tile_T.jpg"  } ;
+scrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4, "image" : "tileImages/scrabble/Scrabble_Tile_U.jpg"  } ;
+scrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_V.jpg"  } ;
+scrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_W.jpg"  } ;
+scrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1, "image" : "tileImages/scrabble/Scrabble_Tile_X.jpg"  } ;
+scrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_Y.jpg"  } ;
+scrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1, "image" : "tileImages/scrabble/Scrabble_Tile_Z.jpg"  } ;
+scrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2, "image" : "tileImages/scrabble/Scrabble_Tile_Blank.jpg"  } ;
+/* Data Structure containing scrabble Tiles gotten from the graphics_data */
+
+/* Each of the droppable functions, they have basic functionality */
+
+$(document).ready(function(){
+    $("#restartButton").click(function(){
+        document.location.reload();
+    });
+
+    $("#nextWord").click(function(){
+
+    });
+
+    $(function() {
+        $("#draggableTileOne").draggable({
+            revert: "valid"
+        });
+        $("#squareOne").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileTwo").draggable({
+            revert: "valid"
+        });
+        $("#squareTwo").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileThree").draggable({
+            revert: "valid"
+        });
+        $("#squareThree").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileFour").draggable({
+            revert: "valid"
+        });
+        $("#squareFour").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileFive").draggable({
+            revert: "valid"
+        });
+        $("#squareFive").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileSix").draggable({
+            revert: "valid"
+        });
+        $("#squareSix").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+
+    $(function() {
+        $("#draggableTileSeven").draggable({
+            revert: "valid"
+        });
+        $("#squareSeven").droppable({
+            drop: function(event, ui) {
+                
+            }
+        });
+      } 
+    );
+    
+});
